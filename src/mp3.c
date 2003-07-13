ae388003ef3854f2228a8c46be792d5a70433d69
brendan <brendan>
2003-07-13 16:31:20 +0000

    Insert annoying LGPL and copyright info into source and include files. The convenience libs are GPLed rather than LGPLed. Is this an issue?
    svn path=/trunk/libshout/; revision=5132

--- src/mp3.c
+++ src/mp3.c
@@ -1,5 +1,22 @@
 /* -*- c-basic-offset: 8; -*- */
-/* mp3.c: libshout MP3 format handler */
+/* mp3.c: libshout MP3 format handler
+ *
+ *  Copyright (C) 2002-2003 the Icecast team <team@icecast.org>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public
+ *  License along with this library; if not, write to the Free
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
