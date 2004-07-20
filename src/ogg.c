0c317929d15699b1c0dff402ed22543c96092624
brendan <brendan>
2004-07-20 02:52:32 +0000

    Move vorbis codec handler into separate file. Theora will be moved shortly.
    svn path=/icecast/trunk/libshout/; revision=7181

--- src/ogg.c
+++ src/ogg.c
@@ -1,5 +1,5 @@
 /* -*- c-basic-offset: 8; -*- */
-/* vorbis.c: Ogg Vorbis data handlers for libshout
+/* ogg.c: Generic ogg data handler
  * $Id$
  *
  *  Copyright (C) 2002-2004 the Icecast team <team@icecast.org>
@@ -27,39 +27,20 @@
 #include <string.h>
 
 #include <ogg/ogg.h>
-#include <vorbis/codec.h>
 #ifdef HAVE_THEORA
 #include <theora/theora.h>
 #endif
 
 #include <shout/shout.h>
 #include "shout_private.h"
+#include "shout_ogg.h"
 
 /* -- local datatypes -- */
-typedef struct _ogg_codec_tag {
-	ogg_stream_state os;
-
-	unsigned int headers;
-	uint64_t senttime;
-
-	void *codec_data;
-	int (*read_page)(struct _ogg_codec_tag *codec, ogg_page *page);
-	void (*free_data)(void *codec_data);
-
-	struct _ogg_codec_tag *next;
-} ogg_codec_t;
-
 typedef struct {
 	ogg_sync_state oy;
 	ogg_codec_t *codecs;
 	char bos;
 } ogg_data_t;
-
-typedef struct {
-	vorbis_info vi;
-	vorbis_comment vc;
-	int prevW;
-} vorbis_data_t;
 
 #ifdef HAVE_THEORA
 typedef struct {
@@ -78,15 +59,8 @@ static void free_codec(ogg_codec_t *codec);
 static void free_codecs(ogg_data_t *ogg_data);
 static int send_page(shout_t *self, ogg_page *page);
 
-/* vorbis handler */
-static int open_vorbis(ogg_codec_t *codec, ogg_page *page);
-static int read_vorbis_page(ogg_codec_t *codec, ogg_page *page);
-static void free_vorbis_data(void *codec_data);
-static int vorbis_blocksize(vorbis_data_t *vd, ogg_packet *p);
-
 #ifdef HAVE_THEORA
 /* theora handler */
-static int open_theora(ogg_codec_t *codec, ogg_page *page);
 static int read_theora_page(ogg_codec_t *codec, ogg_page *page);
 static void free_theora_data(void *codec_data);
 static int theora_ilog(unsigned int v);
@@ -241,83 +215,10 @@ static int send_page(shout_t *self, ogg_page *page)
 	return SHOUTERR_SUCCESS;
 }
 
-/* -- vorbis functions -- */
-static int open_vorbis(ogg_codec_t *codec, ogg_page *page)
-{
-	vorbis_data_t *vorbis_data = calloc(1, sizeof(vorbis_data_t));
-	ogg_packet packet;
-
-	if (!vorbis_data)
-		return SHOUTERR_MALLOC;
-
-	vorbis_info_init(&vorbis_data->vi);
-	vorbis_comment_init(&vorbis_data->vc);
-
-	ogg_stream_packetout(&codec->os, &packet);
-
-	if (vorbis_synthesis_headerin(&vorbis_data->vi, &vorbis_data->vc, &packet) < 0) {
-		free_vorbis_data(vorbis_data);
-		
-		return SHOUTERR_UNSUPPORTED;
-	}
-
-	codec->codec_data = vorbis_data;
-	codec->read_page = read_vorbis_page;
-	codec->free_data = free_vorbis_data;
-
-	return SHOUTERR_SUCCESS;
-}
-
-static int read_vorbis_page(ogg_codec_t *codec, ogg_page *page)
-{
-	ogg_packet packet;
-	vorbis_data_t *vorbis_data = codec->codec_data;
-	uint64_t samples = 0;
-
-	if (codec->headers < 3) {
-		while (ogg_stream_packetout (&codec->os, &packet) > 0) {
-			if (vorbis_synthesis_headerin(&vorbis_data->vi, &vorbis_data->vc, &packet) < 0)
-				return SHOUTERR_INSANE;
-			codec->headers++;
-		}
-
-		return SHOUTERR_SUCCESS;
-	}
-
-	while (ogg_stream_packetout (&codec->os, &packet) > 0)
-		samples += vorbis_blocksize(vorbis_data, &packet);
-
-	codec->senttime += ((samples * 1000000) / vorbis_data->vi.rate);
-
-	return SHOUTERR_SUCCESS;
-}
-
-static void free_vorbis_data(void *codec_data)
-{
-	vorbis_data_t *vorbis_data = (vorbis_data_t *)codec_data;
-
-	vorbis_info_clear(&vorbis_data->vi);
-	vorbis_comment_clear(&vorbis_data->vc);
-	free(vorbis_data);
-}
-
-static int vorbis_blocksize(vorbis_data_t *vd, ogg_packet *p)
-{
-	int this = vorbis_packet_blocksize(&vd->vi, p);
-	int ret = (this + vd->prevW)/4;
-
-	if(!vd->prevW) {
-		vd->prevW = this;
-		return 0;
-	}
-
-	vd->prevW = this;
-	return ret;
-}
 
 #ifdef HAVE_THEORA
 /* theora handler */
-static int open_theora(ogg_codec_t *codec, ogg_page *page)
+int open_theora(ogg_codec_t *codec, ogg_page *page)
 {
 	ogg_packet packet;
 
