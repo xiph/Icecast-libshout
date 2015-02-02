e5a2be3867642b066715ecaebe9d6ddfcc9a3847
Philipp Schafft <lion@lion.leolix.org>
2015-02-02 09:41:07 +0000

    Feature: Added Upgrade: support RFC2817.
    See: #2152
    
    PS: TLS seems to not work when hostnames are used. Why?

--- src/shout.c
+++ src/shout.c
@@ -59,6 +59,7 @@ static int try_write (shout_t *self, const void *data, size_t len);
 
 static int create_request(shout_t *self);
 static int create_http_request(shout_t *self);
+static int create_http_request_upgrade(shout_t *self, const char *proto);
 static int create_xaudiocast_request(shout_t *self);
 static int create_icy_request(shout_t *self);
 static int parse_response(shout_t *self);
@@ -1164,12 +1165,45 @@ retry:
 
 	case SHOUT_STATE_TLS_PENDING:
 #ifdef HAVE_OPENSSL
-		if (self->tls_mode != SHOUT_TLS_DISABLED) {
+		switch (self->tls_mode) {
+		case SHOUT_TLS_DISABLED:
+			/* nothing to be done */
+		break;
+		case SHOUT_TLS_AUTO:
+		case SHOUT_TLS_AUTO_NO_PLAIN:
+			if (self->server_caps & LIBSHOUT_CAP_GOTCAPS) {
+				/* We had a probe allready, otherwise just do nothing to poke the server. */
+				if (self->server_caps & LIBSHOUT_CAP_UPGRADETLS) {
+					self->tls_mode = SHOUT_TLS_UPGRADE;
+				} else {
+					if (self->tls_mode == SHOUT_TLS_AUTO_NO_PLAIN)
+						return SHOUTERR_NOTLS;
+					self->tls_mode = SHOUT_TLS_DISABLED;
+				}
+				self->state = SHOUT_STATE_TLS_PENDING;
+				goto retry;
+			}
+		break;
+		case SHOUT_TLS_OVER_TLS:
 			if ((rc = shout_tls_try_connect(self)) != SHOUTERR_SUCCESS) {
 				if (rc == SHOUTERR_BUSY)
 					return SHOUTERR_BUSY;
 				goto failure;
 			}
+		break;
+		case SHOUT_TLS_UPGRADE:
+			if ((rc = create_http_request_upgrade(self, "TLS/1.0")) != SHOUTERR_SUCCESS) {
+				if (rc == SHOUTERR_BUSY)
+					return SHOUTERR_BUSY;
+				goto failure;
+			}
+			self->state = SHOUT_STATE_REQ_PENDING;
+			goto retry;
+		break;
+		default:
+                        rc = SHOUTERR_INSANE;
+                        goto failure;
+		break;
 		}
 #endif
 		self->state = SHOUT_STATE_REQ_CREATION;
@@ -1439,6 +1473,24 @@ static int create_http_request(shout_t *self)
 	return ret;
 }
 
+static int create_http_request_upgrade(shout_t *self, const char *proto)
+{
+	do {
+		if (queue_str(self, "GET / HTTP/1.1\r\nConnection: Upgrade\r\n"))
+			break;
+		if (queue_printf(self, "Upgrade: %s\r\n", proto))
+			break;
+		/* Send Host:-header as this one may be used to select cert! */
+		if (queue_printf(self, "Host: %s:%i\r\n", self->host, self->port))
+			break;
+		if (queue_str(self, "\r\n"))
+			break;
+		return SHOUTERR_SUCCESS;
+	} while (0);
+
+	return SHOUTERR_MALLOC;
+}
+
 static char *http_basic_authorization(shout_t *self)
 {
 	char *out, *in;
@@ -1526,6 +1578,50 @@ static inline void parse_http_response_caps(shout_t *self, const char *header, c
 	return;
 }
 
+static inline int eat_body(shout_t *self, ssize_t len, const char *buf, size_t buflen)
+{
+	const char *p;
+	size_t header_len = 0;
+	char buffer[256];
+	ssize_t got;
+
+	if (!len)
+		return 0;
+
+	for (p = buf; p < (buf+buflen-3); p++) {
+		if (p[0] == '\r' && p[1] == '\n' && p[2] == '\r' && p[3] == '\n') {
+			header_len = p - buf + 4;
+			break;
+		} else if (p[0] == '\n' && p[1] == '\n') {
+			header_len = p - buf + 2;
+			break;
+		}
+	}
+	if (!header_len && buflen >= 3 && buf[buflen-2] == '\n' && buf[buflen-3] == '\n') {
+		header_len = buflen - 1;
+	} else if (!header_len && buflen >= 2 && buf[buflen-1] == '\n' && buf[buflen-2] == '\n') {
+		header_len = buflen;
+	}
+
+	if ( (buflen - header_len) > len)
+		return -1;
+
+	len -= buflen - header_len;
+
+	while (len) {
+		got = conn_read(self, buffer, len > sizeof(buffer) ? sizeof(buffer) : len);
+		if (got == -1 && conn_recoverable(self)) {
+			continue;
+		} else if (got == -1) {
+			return -1;
+		}
+
+		len -= got;
+	}
+
+	return 0;
+}
+
 static int parse_http_response(shout_t *self)
 {
 	http_parser_t *parser;
@@ -1548,7 +1644,7 @@ static int parse_http_response(shout_t *self)
 		 */
 		parse_http_response_caps(self, "Allow", httpp_getvar(parser, "allow"));
 		parse_http_response_caps(self, "Accept-Encoding", httpp_getvar(parser, "accept-encoding"));
-		parse_http_response_caps(self, "Upgrade", httpp_getvar(parser, "ullow"));
+		parse_http_response_caps(self, "Upgrade", httpp_getvar(parser, "upgrade"));
 		self->server_caps |= LIBSHOUT_CAP_GOTCAPS;
 		retcode = httpp_getvar(parser, HTTPP_VAR_ERROR_CODE);
 		code = atoi(retcode);
@@ -1556,8 +1652,18 @@ static int parse_http_response(shout_t *self)
 			httpp_destroy(parser);
 			free (header);
 			return SHOUTERR_SUCCESS;
-		} else if (code == 401 || code == 405) {
-			/* TODO: we should add "426 Upgrade Required" to the list once we support upgrade protocol */
+		} else if (code == 401 || code == 405 || code == 426 || code == 101) {
+			const char *content_length = httpp_getvar(parser, "content-length");
+			if (content_length) {
+				if (eat_body(self, atoi(content_length), header, hlen) == -1)
+					goto failure;
+			}
+#ifdef HAVE_OPENSSL
+			switch (code) {
+			case 426: self->tls_mode = SHOUT_TLS_UPGRADE;  break;
+			case 101: self->tls_mode = SHOUT_TLS_OVER_TLS; break;
+			}
+#endif
 			self->retry++;
 			if (self->retry > LIBSHOUT_MAX_RETRY)
 				self->retry = 0;
@@ -1566,6 +1672,7 @@ static int parse_http_response(shout_t *self)
 		}
 	}
 
+failure:
 	free(header);
 	httpp_destroy(parser);
 	return self->error = SHOUTERR_NOLOGIN;
